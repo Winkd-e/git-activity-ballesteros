20deaded413b3c39e5f3a4ce795fd30ae656d00e
Winkd-e <ballesteroswb.844.stud@cdd.edu.ph>
2024-07-18 13:23:36 +0800

    Update index.php
    adfa

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Khrizzle</title>
+    <title>Khrizzle Bagsak!!!</title>
 </head>
 <body>
     Alliah Carrie
